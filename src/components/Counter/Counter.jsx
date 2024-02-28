ef11ac90edabc357758e64d70c0f1e0220494dd7
annurtasnimislam <annurtasnimislam@gmail.com>
2024-02-28 16:11:20 +0600

    install: redux

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,18 @@
+// import { useDispatch, useSelector } from "react-redux";
+// import { increment, decrement } from "../features/counterSlice";
+
+const Counter = () => {
+  //   const dispatch = useDispatch();
+  //   const counter = useSelector((state) => state.counter);
+
+  return (
+    <div>
+      {/* <p>Count: {counter}</p> */}
+      {/* <button onClick={() => dispatch(increment())}>Increment</button> */}
+      {/* <button onClick={() => dispatch(decrement())}>Decrement</button> */}
+      <p>pppppp</p>
+    </div>
+  );
+};
+
+export default Counter;
